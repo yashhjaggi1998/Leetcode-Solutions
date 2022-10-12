b6b97dc318fcfd8186aedd07fd39784e1ffb0c27
Yashh Jaggi <32009709+yashhjaggi1998@users.noreply.github.com>
2022-10-11 18:59:15 -0700

    Create kthFactorOfn.java

---
+++ kthFactorOfn.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/the-kth-factor-of-n/
+
+class Solution {
+    public int kthFactor(int n, int k) 
+    {
+        int kCopy = 0;
+       for(int j=1; j<=n ; j++)
+       {
+            if(n%j == 0)
+            {
+                kCopy++;
+                if(kCopy == k)
+                    return j;
+            }
+       }
+        return -1;
+    }
+}
