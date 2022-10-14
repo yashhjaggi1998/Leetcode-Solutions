662e67feccc25424bd553f8a24f8d4d8a3ecde02
Yashh Jaggi <32009709+yashhjaggi1998@users.noreply.github.com>
2022-10-14 15:50:56 -0700

    Create tribonacci.java

---
+++ DP/tribonacci.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n <= 1)
+            return n;
+            
+        int[] T = new int[n+1];
+        
+        T[0] = 0;
+        T[1] = 1;
+        T[2] = 1;
+        for(int i=3; i<=n; i++)
+            T[i] = T[i-2] + T[i-1] + T[i-3];
+        return T[n];
+    }
+}
